93006d4da59559fbe86459cec8dc8636b9f3c5e8
hamza-smh <63658773+hamza-smh@users.noreply.github.com>
2023-02-11 00:42:13 +0500

    Twelth Commit
    Responsive Menu + Responsive site
    -Dropdown

--- src/component/navigation/navigation.jsx
+++ src/component/navigation/navigation.jsx
@@ -12,19 +12,22 @@ const Navigation = () =>{
             <div className="container">
               <div className="row">
                 <img className=" logo" src = {logo} />
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
+                
+                <button className="navbar-toggler" type="button"  data-toggle="collapse" data-target="#navbarSupportedContent" 
+                        aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                
                 </button>
               </div>
               <div role = "navigation" >
-                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     < ul className = "navlinkBar navbar-nav mr-auto" >
                         <li className="nav-item"><a className="nav-link scrollto active"  href = '/home'>Home</a></li>
                         <li className="nav-item"><a className="nav-link scrollto"  href = '/about'>About       </a></li>
                         <li className="nav-item"><a className="nav-link scrollto"  href = '/services'>Services    </a></li>
                         <li className="nav-item"><a className="nav-link scrollto"  href = '/portfolio'>Portfolio   </a></li>
                         <li className="nav-item"><a className="nav-link scrollto"  href = '/team'>Team        </a></li>
-                        <li className = "nav-item " >
+                        {/* <li className = "nav-item " >
                             <div className="dropdown">
                                 <button className="nav-link dropbtn">DropDown    
                                   <i className="fa fa-caret-down"></i>
@@ -35,11 +38,12 @@ const Navigation = () =>{
                                   <a href="#">Link 3</a>
                                 </div>
                             </div>   
-                        </li> 
+                        </li>  */}
 
 
-                            {/* <li class="nav-item dropdown">
-                              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                            <li class="nav-item dropdown">
+                              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" 
+                                  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
                                 Dropdown
                               </a>
                               
@@ -49,7 +53,7 @@ const Navigation = () =>{
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="#">Something else here</a>
                               </div>
-                            </li> */}
+                            </li>
                             
 
 
@@ -59,6 +63,7 @@ const Navigation = () =>{
                            <GetStartedButton buttonType= "transparent" />
                            </a> </li> 
                     </ul>
+                    <i class="bi bi-list mobile-nav-toggle"></i>
                   </div>
               </div> 
             </div>  
