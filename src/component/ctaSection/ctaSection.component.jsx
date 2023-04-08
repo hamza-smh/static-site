c57629935843cd24317a7f563dea128fa2f74392
hamza-smh <hamza.smh2000@gmail.com>
2023-04-08 13:19:28 +0500

    Responsive Phase 2

--- src/component/ctaSection/ctaSection.component.jsx
+++ src/component/ctaSection/ctaSection.component.jsx
@@ -6,7 +6,7 @@ const CTASection = () =>{
             <section id="cta" className="cta">
                 <div className="container" data-aos="zoom-in">
                     <div className="row">
-                        <div className="col-lg-9 text-lg-start">
+                        <div className="holder col-lg-9 ">
                             <h3>Call To Action</h3>
                             <p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                         </div>
--- src/component/servicesSection/serviceCard/serviceCard.component.jsx
+++ src/component/servicesSection/serviceCard/serviceCard.component.jsx
@@ -3,8 +3,9 @@ import "./serviceCard.styles.scss";
 const ServiceCard = ({data,index}) =>{
 
     const {title,icon,text}=data;
+    //col-xl-3 col-md-6
     return(
-        <div className="card col-xl-3 col-md-6 ">
+        <div className="card  ">
             <div className="icon-box">
                 <div className="icon">
                     <i>{icon}</i>
