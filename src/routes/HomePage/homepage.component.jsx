2c80880e979b88b60d1ff5d0e63ce72db1371286
hamza-smh <hamza.smh2000@gmail.com>
2023-06-09 15:52:41 +0500

    STH wrong

--- src/routes/HomePage/homepage.component.jsx
+++ src/routes/HomePage/homepage.component.jsx
@@ -3,18 +3,12 @@ import React from "react";
 import Container from "../../component/container/container.component";
 import Client from "../../component/clients/clients.component";
 import AboutUs from "../../component/AboutUs/aboutUs.component";
-import LearnMore from "../../component/LearnMore/learnMore.component";
-import SkillSection from "../../component/skills-section/skills.component";
 import Services from "../../component/servicesSection/services.component";
 import WhyUs from "../../component/WhyUs/whyus.component";
 import Achievements from "../../component/Acheivments/Achievements.component";
 import Feature from "../../component/Feature Projects/feature.component";
 import Testimonial from "../../component/Testimonials/testimonial.component";
-import CTASection from "../../component/ctaSection/ctaSection.component";
-import Portfolio from "../../component/portfolio/portfolio.scomponent";
-import Team from "../../component/team/team.component";
 import Pricing from "../../component/pricing/pricing.component";
-import FAQ from "../../component/faq/faq.component";
 import Contact from "../../component/contact/contact.component";
 import Footer from "../../component/footer/footer.component";
 
@@ -26,19 +20,19 @@ const HomePage =() =>{
         <>
             <Container />
             <Client />
-            {/* <LearnMore /> */}
+            
             <AboutUs />
-            {/* <SkillSection /> */}
+            
             <Services />
             <WhyUs />
             <Achievements />
             <Feature />
             <Testimonial />
-            {/* <CTASection /> */}
-             {/* <Portfolio /> */}
-            {/* <Team /> */}
+            
+             
+            
             {/* <Pricing /> */}
-            {/* <FAQ /> */}
+            
             <Contact />
             <Footer /> 
         </>
